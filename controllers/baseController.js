eedf1329da791efb897cd500a7fff6d2391aee9f
rorellana-b <ronalbalmoreorellanabonilla@192.168.1.2>
2025-02-14 22:50:09 -0600

    fixed basecontroller

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -4,7 +4,6 @@ const inventoryModel = require("../models/inventory-model");
 
 baseController.buildHome = async function (req, res) {
     const nav = await utilities.getNav();
-    req.flash("notice", "This is a flash message.")
     res.render("index", {
         title: "Home",
         nav,
