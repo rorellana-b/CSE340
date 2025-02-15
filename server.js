e90a207d4a7bc5ce374744a5709d427756ea6c8e
rorellana-b <ronalbalmoreorellanabonilla@192.168.1.2>
2025-02-14 22:25:48 -0600

    fixed server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@
  *************************/
 const session = require("express-session")
 const flash = require('connect-flash')
-// const pool = require('./database')
+const pool = require('./database')
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const env = require("dotenv").config();
